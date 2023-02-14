72923b42483f9aadb1cbd6498c0ebd4de4f7a5fa
SwapnilNair <swapnilnair747@gmail.com>
2023-02-14 15:23:10 +0530

    Pushing the code

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Swapnil has successfully built and run this thing\n";
+  return 0;
+}
